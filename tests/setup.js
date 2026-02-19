2ea1ca9b47f698c4777f3af497b4a74a8f696986
housemouse62 <ryan.skeels@gmail.com>
2026-02-19 22:44:21 +0100

    Set up testing environment, all haiku tests passing

---
+++ tests/setup.js
@@ -0,0 +1,33 @@
+import { expect, afterEach, beforeEach, vi } from "vitest";
+import { cleanup } from "@testing-library/react";
+import * as matchers from "@testing-library/jest-dom/matchers";
+
+expect.extend(matchers);
+
+// Create a real implementation of localStorage for tests
+const localStorageMock = (() => {
+  let store = {};
+
+  return {
+    getItem: (key) => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = value.toString();
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+  };
+})();
+
+globalThis.localStorage = localStorageMock;
+
+beforeEach(() => {
+  localStorage.clear();
+});
+
+afterEach(() => {
+  cleanup();
+});
