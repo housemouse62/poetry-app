ef0d4e09e99a1d56a2035e36a16d1b4e31c580bb
housemouse62 <ryan.skeels@gmail.com>
2026-02-21 17:28:19 +0100

    dashboard functional a roughly styled

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,10 +6,21 @@ export default function Root() {
     <>
       <div className="root-app">
         <div class="root-container">
-          <h2>hello world. we do poetry.</h2>
-          <Link to="/haiku">haiku</Link>
-          <br />
-          <Link to="/limerick">limerick</Link>
+          <div className="root-title-div">
+            <h1 class-Name="root-title">let's make poetry.</h1>
+          </div>
+          <div className="poem-cards">
+            <div className="poem-card haiku">
+              <Link to="/haiku" className="link">
+                haiku
+              </Link>
+            </div>
+            <div className="poem-card limerick">
+              <Link to="/limerick" className="link">
+                limerick
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
