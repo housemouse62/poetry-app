9ef307db884949eb3fe006b48b3616858a9aac10
housemouse62 <ryan.skeels@gmail.com>
2026-05-29 22:38:19 +0200

    build haiky and limerick reply routes

--- apps/api/app.js
+++ apps/api/app.js
@@ -7,6 +7,8 @@ import haikuRouter from "./src/haiku.js";
 import limerickRouter from "./src/limerick.js";
 import haikuCommentRouter from "./src/haikuComment.js";
 import limerickCommentRouter from "./src/limerickComment.js";
+import haikuReplyRouter from "./src/haikuReply.js";
+import limerickReplyRouter from "./src/limerickReply.js";
 
 const app = express();
 
@@ -21,6 +23,8 @@ app.use("/haiku", haikuRouter);
 app.use("/limerick", limerickRouter);
 app.use("/haikuComment", haikuCommentRouter);
 app.use("/limerickComment", limerickCommentRouter);
+app.use("/haikuReply", haikuReplyRouter);
+app.use("/limerickReply", limerickReplyRouter);
 
 app.listen(process.env.PORT, () =>
   console.log(`Listening on port ${process.env.PORT}`),
