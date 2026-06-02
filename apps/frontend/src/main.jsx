7b51ae9e78943f0e856f952c4da0d5ba3ae3d7bf
housemouse62 <ryan.skeels@gmail.com>
2026-06-02 09:41:39 +0200

    build out authContext and protected routes

--- apps/frontend/src/main.jsx
+++ apps/frontend/src/main.jsx
@@ -3,11 +3,14 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import routes from "./routes.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 );
--- apps/frontend/src/routes.jsx
+++ apps/frontend/src/routes.jsx
@@ -1,7 +1,11 @@
 import Root from "./routes/root";
+import Register from "./pages/Register/Register.jsx";
+import Login from "./pages/Login/Login.jsx";
 import HaikuApp from "./pages/HaikuApp/HaikuApp";
 import LimerickApp from "./pages/Limerick/LimerickApp";
 import ErrorPage from "./errorPage";
+
+import { ProtectedRoute } from "./components/ProtectedRoute.jsx";
 
 const routes = [
   {
@@ -10,14 +14,29 @@ const routes = [
     errorElement: <ErrorPage />,
   },
   {
-    path: "/haiku",
-    element: <HaikuApp />,
+    path: "/register",
+    element: <Register />,
     errorElement: <ErrorPage />,
   },
   {
-    path: "/limerick",
-    element: <LimerickApp />,
+    path: "/login",
+    element: <Login />,
     errorElement: <ErrorPage />,
+  },
+  {
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/haiku",
+        element: <HaikuApp />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/limerick",
+        element: <LimerickApp />,
+        errorElement: <ErrorPage />,
+      },
+    ],
   },
 ];
 
